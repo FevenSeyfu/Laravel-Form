c065c741c893be7325f081ff5811484128594426
FevenSeyfu <fevenseyfu00@gmail.com>
2024-08-14 19:47:05 +0300

    add click event to edit button to make inout editable and update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap';
 import '../css/app.css';
+
+document.querySelectorAll('.btn-edit').forEach(button => {
+    button.addEventListener("click", function () {
+        const row = this.closest("tr");
+        row.querySelectorAll("input").forEach((input) =>
+            input.removeAttribute("readonly")
+        );
+        row.querySelector(".btn-update").style.display = "inline-block";
+        this.style.display = "none";
+    });
+});
